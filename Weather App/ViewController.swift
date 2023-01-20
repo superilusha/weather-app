c6ffddcf01edd4bdf38a49649799d7acfb90ad72
Ilya Anoshin <anoshin.ilya@gmail.com>
2023-01-20 19:22:27 +0200

    Initail commit with Main.storyboard label/button/image skeleton

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Илья Аношин on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
