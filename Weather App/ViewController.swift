ad6533d9b358b7a7d2540b63dd64683b49032c58
Ilya Anoshin <anoshin.ilya@gmail.com>
2023-01-23 15:45:18 +0200

    dev commit

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    @IBOutlet weak var searchPressed: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
