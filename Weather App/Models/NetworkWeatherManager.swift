d83dc2e979ec2d76cf6420a6443bf343887bf16c
Ilya Anoshin <anoshin.ilya@gmail.com>
2023-01-25 18:08:03 +0200

    add image set, and city change

--- Weather App/Models/NetworkWeatherManager.swift
+++ Weather App/Models/NetworkWeatherManager.swift
@@ -7,17 +7,22 @@
 
 import Foundation
 
-//Создаю структуру-менеджера с методом для создания сесии и получения json-файла
+//Создаю class-менеджер с методом для создания сесии и получения json-файла
 
-struct NetworkWeatherManager {
+class NetworkWeatherManager {
+    
+    var onCompletion: ((CurrentWeather) -> Void)?
+    
     
     func fetchCurrentWeather(forCity city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
         guard let url = URL(string: urlString) else {return}
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data{
-                let currentWeather = self.parseJson(withData: data)
+                if let currentWeather = self.parseJson(withData: data) {
+                    self.onCompletion?(currentWeather)
+                }
             }
         }
         task.resume()
--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -18,20 +18,42 @@ class ViewController: UIViewController {
     
     var networkWeatherManager = NetworkWeatherManager()
     
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        networkWeatherManager.onCompletion = { [weak self] currentWeather in
+            guard let self = self else {return}
+            self.updateInterfaceWith(weather: currentWeather)
+        }
         networkWeatherManager.fetchCurrentWeather(forCity: "Moscow")
         
+    
+        
     }
+    
+    
+    // функция для изменения элементов интерфейса. Исполнение функции в главном потоке
+    func updateInterfaceWith(weather: CurrentWeather) {
+        
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            self.feelsLikeTemperatureLabel.text = weather.feelsLikeTemperatureString
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
+        }
+    }
+    
     
     
     
     // привязываю аутлет кнопки и вешаю на нее алерт
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [unowned self] city in
             self.networkWeatherManager.fetchCurrentWeather(forCity: city)
-            
         }
     }
     
